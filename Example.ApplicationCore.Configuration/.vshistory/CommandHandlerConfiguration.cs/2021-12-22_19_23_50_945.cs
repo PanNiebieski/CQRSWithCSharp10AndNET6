dc65c6569cfd4f1d390525f4dcb710b2fe483016
Cezary Walenciuk <cezarywalenciuk@gmail.com>
2021-12-30 06:51:57 +0100

    delete vshistory

---
+++ Example.ApplicationCore.Configuration/.vshistory/CommandHandlerConfiguration.cs/2021-12-22_19_23_50_945.cs
@@ -0,0 +1,42 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.ApplicationCore.Configuration
+{
+    public static class CommandHandlerConfiguration
+    {
+        public static IServiceCollection AddCommandHandler
+            <TCommand, TCommandHandler>(
+            this IServiceCollection services
+        ) where TCommandHandler : class, ICommandHandler<TCommand>
+        {
+
+            services.AddTransient<ICommandHandler<TCommand>, TCommandHandler>()
+                    .AddTransient<CommandHandler<TCommand>>(
+                        sp => sp.GetRequiredService
+                        <ICommandHandler<TCommand>>().Handle
+                );
+
+            return services;
+        }
+
+        //public static IServiceCollection AddCommandHandler
+        //    <AddPostCommand, AddPostCommandHandler>(
+        //    this IServiceCollection services
+        //    ) where AddPostCommandHandler : class, ICommandHandler<TCommand>
+        //{
+
+        //    services.AddTransient<ICommandHandler<AddPostCommand>, AddPostCommandHandler>();
+
+        //    services.AddTransient<CommandHandler<AddPostCommand>>(
+        //                sp => sp.GetRequiredService
+        //                <ICommandHandler<AddPostCommand>>().Handle
+        //        );
+
+        //    return services;
+        //}
+    }
+
+    public delegate ValueTask
+        CommandHandler<in TCommand>(TCommand command, CancellationToken ct);
+}
---
+++ Example.ApplicationCore.Configuration/.vshistory/QueryHandlerConfiguration.cs/2021-12-22_19_24_16_121.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.ApplicationCore.Configuration
+{
+    public static class QueryHandlerConfiguration
+    {
+        public static IServiceCollection AddQueryHandler
+            <TQuery, TResult, TQueryHandler>(
+            this IServiceCollection services
+        ) where TQueryHandler : class, IQueryHandler<TQuery, TResult>
+        {
+            services
+                .AddTransient<IQueryHandler<TQuery, TResult>, TQueryHandler>()
+                .AddTransient<QueryHandler<TQuery, TResult>>(
+                    sp => sp.GetRequiredService<IQueryHandler<TQuery, TResult>>
+                    ().Handle
+            );
+
+            return services;
+        }
+
+        //public static IServiceCollection AddQueryHandler
+        //    <GetAllPostsQuery, List<Post>, GetAllPostsQueryHandler>(
+        //    this IServiceCollection services
+        //    ) where GetAllPostsQueryHandler : class, IQueryHandler<TQuery, TResult>
+        //{
+        //    services
+        //        .AddTransient<IQueryHandler<GetAllPostsQuery, List<Post>>,
+        //        GetAllPostsQueryHandler>();
+
+        //    services.AddTransient<QueryHandler<GetAllPostsQuery, List<Post>>>(
+        //            sp => sp.GetRequiredService<IQueryHandler<GetAllPostsQuery, List<Post>>>
+        //            ().Handle
+        //    );
+
+        //    return services;
+        //}
+    }
+
+    public delegate ValueTask<TResult>
+        QueryHandler<in TQuery, TResult>(TQuery query, CancellationToken ct);
+}
